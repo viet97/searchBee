71a9c7ca3456d61b4330338eaffb9b133dec1734
VietDQ <vietdq@VietDQs-iMac.local>
2022-04-27 00:44:55 +0700

    done login screen

--- assets/Colors/index.js
+++ assets/Colors/index.js
@@ -3,7 +3,7 @@ export default {
     background_droppicker: 'rgba(0,0,0,0.8)',
     STATUS_BAR_COLOR: '#00000069',
     black: '#000',
-    white: '#fff',
+    white: '#ffffff',
     backgroundScreen: '#141414',
     blackAlpha: '#00000069',
     price: '#ffb813',
--- src/components/screen/LoginScreen/index.js
+++ src/components/screen/LoginScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Pressable } from 'react-native';
 
 import { connect } from 'react-redux';
 
@@ -8,6 +8,8 @@ import Colors from '../../../../assets/Colors';
 import CustomText from '../../common/Text';
 import InputField from '../../common/InputField';
 import SVGIcon from '../../../../assets/SVGIcon';
+import SBButton from '../../common/SBButton';
+import { insets } from '../../../utils/DeviceUtil';
 
 class LoginScreen extends BaseScreen {
   constructor(props) {
@@ -18,43 +20,100 @@ class LoginScreen extends BaseScreen {
     return (
       <View style={styles.container}>
         <View
-          style={{
-            paddingHorizontal: 30,
-            alignItems: 'center'
-          }}>
+          style={styles.contentContainer}>
+          <View
+            style={styles.underLogo}>
+            <SVGIcon.splash_logo
+              width={232}
+              height={280}
+            />
+          </View>
           <CustomText
             bold
             size={24}
-            style={{
-              marginTop: 100,
-              lineHeight: 36,
-            }}>
+            style={styles.welcome}>
             welcome to Search Bee
           </CustomText>
           <CustomText
             size={16}
-            style={{
-              marginTop: 4,
-              lineHeight: 24
-            }}>
+            style={styles.title}>
             Insert Email and Password
           </CustomText>
 
           <InputField
-            containerStyle={{
-              marginTop: 60
-            }}
+            containerStyle={styles.username}
             placeholder={"insert email or mobile number"}
             Icon={SVGIcon.username}
           />
           <InputField
-            containerStyle={{
-              marginTop: 30
-            }}
+            containerStyle={styles.password}
             placeholder={"insert password here"}
             Icon={SVGIcon.password}
             secureTextEntry
           />
+          <SBButton
+            hitSlop={8}
+            style={styles.forgotPassword}>
+            <CustomText
+              bold
+              size={12}
+              style={{
+                lineHeight: 18,
+              }}>
+              Forgot Password?
+            </CustomText>
+          </SBButton>
+
+          <SBButton
+            hitSlop={8}
+            style={styles.signin}>
+            <CustomText
+              bold
+              size={14}
+              style={{
+                lineHeight: 21,
+              }}>
+              Sign In
+            </CustomText>
+          </SBButton>
+          <CustomText
+            size={11}
+            style={styles.loginWith}>
+            or login with
+          </CustomText>
+          <View
+            style={styles.socialLoginRow}>
+            <SBButton
+              style={{
+                marginRight: 30
+              }}>
+              <SVGIcon.gmail
+                width={60}
+                height={60} />
+            </SBButton>
+            <SBButton>
+              <SVGIcon.fb
+                width={60}
+                height={60} />
+            </SBButton>
+          </View>
+          <View
+            style={styles.signupRow}>
+            <CustomText
+              size={14}
+              style={{
+                lineHeight: 21
+              }}>Don’t Have a Account?</CustomText>
+            <SBButton
+              hitSlop={16}>
+              <CustomText
+                bold
+                size={14}
+                style={{
+                  lineHeight: 21
+                }}>{" "}Sign Up</CustomText>
+            </SBButton>
+          </View>
         </View>
       </View>
     );
@@ -77,6 +136,59 @@ export default connect(
 )(LoginScreen);
 
 const styles = StyleSheet.create({
+  signupRow: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    marginBottom: insets.bottom || 24
+  },
+  socialLoginRow: {
+    flexDirection: 'row',
+    marginTop: 16
+  },
+  loginWith: {
+    lineHeight: 17,
+    marginTop: 16
+  },
+  signin: {
+    marginTop: 10,
+    backgroundColor: Colors.prussian_blue,
+    width: '100%',
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginTop: 186
+  },
+  forgotPassword: {
+    alignSelf: 'flex-end',
+    marginTop: 10
+  },
+  password: {
+    marginTop: 30
+  },
+  username: {
+    marginTop: 60
+  },
+  title: {
+    marginTop: 4,
+    lineHeight: 24
+  },
+  welcome: {
+    marginTop: 60,
+    lineHeight: 36,
+  },
+  underLogo: {
+    ...StyleSheet.absoluteFillObject,
+    opacity: 0.3,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  contentContainer: {
+    paddingHorizontal: 30,
+    alignItems: 'center',
+    flex: 1
+  },
   container: {
     flex: 1,
     backgroundColor: Colors.mainColor,
